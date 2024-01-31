316eac0b3acc18cfa48958cc94809dd7ab5864e8
conert1 <conertmavuso@gmail.com>
2024-01-31 16:07:41 +0200

    added tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,24 @@ import scheduler
 
 
 class MyTestCase(unittest.TestCase):
-    def test_date(self):
+    def test_display(self):
         with captured_io(StringIO('a\naa\n')) as (out, err):
             scheduler.Display()
             output = out.getvalue().strip()
             self.assertEqual("", output)
 
+    def test_time(self):
+        with captured_io(StringIO('a\naa\n11:11')) as (out, err):
+            scheduler.time()
+            output = out.getvalue().strip()
+            self.assertEqual("Enter the time\nplease Enter a valid time HH:MM\nplease Enter a valid time HH:MM", output)
+
+
+    def test_date(self):
+        with captured_io(StringIO('136-65-55\n2024-01-01')) as (out, err):
+            scheduler.date()
+            output = out.getvalue().strip()
+            self.assertEqual("enter the date\nSorry invalid date please enter date in the format YYYY-MM-DD", output)
 
 if __name__ == '__main__':
     unittest.main()
